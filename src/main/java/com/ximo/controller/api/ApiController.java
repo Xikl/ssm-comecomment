26e7614320360f30f004cfbee20f15b6a7106d01
X1mo <1392936858@qq.com>
2017-11-25 22:54:26 +0800

    ssm-comment开始

---
+++ src/main/java/com/ximo/controller/api/ApiController.java
@@ -0,0 +1,14 @@
+package com.ximo.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: 朱文赵
+ * @date: 2017/11/25
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+}
