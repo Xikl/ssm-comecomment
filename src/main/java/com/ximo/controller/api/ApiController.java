f2c7b804eac413b3a5cf36ee59807ce0f97f5799
X1mo <1392936858@qq.com>
2017-11-26 14:28:44 +0800

    解决已知bug

--- src/main/java/com/ximo/controller/api/ApiController.java
+++ src/main/java/com/ximo/controller/api/ApiController.java
@@ -1,14 +1,25 @@
 package com.ximo.controller.api;
 
+import com.ximo.domain.Ad;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
- * @description:
+ * @description: apiController
  * @author: 朱文赵
- * @date: 2017/11/25
+ * @date: 2017/11/26
  */
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+    @GetMapping("/homead")
+    public String homead(){
+        return "你好";
+    }
+
+
 }
